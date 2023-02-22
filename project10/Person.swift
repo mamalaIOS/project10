e98cb10f36eb30ff4a699bf77b521f9b97b14ecc
Amel <amelsb45@gmail.com>
2023-02-22 11:11:10 -0500

    Initial Commit

---
+++ project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Amel Sbaihi on 12/9/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name : String
+    var image : String
+    
+    init (name : String , image : String )  {
+        
+        self.name = name
+        self.image = image 
+    }
+
+}
---
+++ project10/PersonCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PersonCollectionViewCell.swift
+//  project10
+//
+//  Created by Amel Sbaihi on 12/8/22.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var label: UILabel!
+    
+    
+    
+}
